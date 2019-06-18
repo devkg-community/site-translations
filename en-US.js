9549cacc7f4d138c318ad4813dd3ab80a38c0d79
slavakim <slava.kim@zensoft.io>
2019-06-18 22:15:58 +0600

    - added eng translations - fixed some russian lang typos

---
+++ en-US.js
@@ -0,0 +1,233 @@
+export default {
+  pages: {
+    homepage: {
+      title: 'DevKG - Developers of Kyrgyzstan',
+      keywords: 'work in Bishkek, developer, event, course, event bishkek, bishkek, Kyrgyzstan, developers, devkg',
+      description: 'DevKG - Kyrgyzstan developers community'
+    },
+    meetups: {
+      title: 'Video conferences - DevKG',
+      keywords: 'online, video lesson, course, meetup, event, howto videos, newbies video, info, information',
+      description: 'Video conference – online meeting for sharing experience between professionals in informal way.'
+    },
+    meetups_query: {
+      title: '{title} - Video conference - DevKG',
+      keywords: 'DevKG, {title}, {organizer}, online, video lesson, course, meetiup, event, howto video, info, information'
+    },
+    jobs: {
+      title: 'Vacancies - DevKG',
+      keywords: 'vacancies, work in IT, remote work, bishkek, remote work, developers of kyrgyzstan, freelance, remote, relocate, offices in bishkek, outsource, out stuff',
+      description: 'Job search in IT. Work in Kyrgyzstan, work in bishkek'
+    },
+    jobs_query: {
+      title: '{position} - {company} - Vacancy - DevKG',
+      keywords: 'DevKG, {position}, {company}, vacancies, work in IT, remote work, bishkek, remote work, developers of kyrgyzstan, freelance, remote, offices in bishkek, outsource, out stuff'
+    },
+    events: {
+      title: 'Events - DevKG',
+      keywords: 'events, events in IT, bishkek, offline events, conferences on bishkek',
+      description: 'Conferences for sharing an experience between developers'
+    },
+    events_query: {
+      title: '{title} - Event - DevKG',
+      keywords: 'DevKG, {title}, {organizer}, events, IT events, bishkek, offline event, conferences in bishkek'
+    },
+    dashboard: {
+      meetups_index: 'Your conferences - DevKG',
+      meetups_create: 'Create new conference- DevKG',
+      jobs_index: 'Your conferences - DevKG',
+      jobs_create: 'Create new vacancy- DevKG',
+      events_index: 'Your events- DevKG',
+      events_create: 'Create new event- DevKG',
+      management_index: 'Management - DevKG',
+      organizations_index: 'Organizations - DevKG',
+      organizations_create: 'Create new organizations - DevKG'
+    },
+    community: {
+      title: 'Community - DevKG',
+      keywords: 'chats list, developers chat, channels, youtube, developers',
+      description: 'Resources list for developers'
+    },
+    error: {
+      404: 'Page that you are looking for, does not exist, please check URL'
+    }
+  },
+  login: {
+    title: 'Login with Telegram:'
+  },
+  general: {
+    goto: 'Go to',
+    up: 'Up',
+    organizer: 'Event manager',
+    from: 'From',
+    to: 'To',
+    fixed: 'Project payment',
+    company: 'Company',
+    position: 'Position',
+    members: 'Members',
+    preview: 'Preview',
+    price: 'Salary',
+    type: 'Type',
+    location: 'Locations',
+    name: 'Name',
+    created_data: 'Creation date',
+    remove: 'Delete',
+    archive: 'Archive',
+    publish: 'Publish',
+    kickback: 'Отозвать',
+    example: 'Example',
+    date: 'Date',
+    spec: 'Description',
+    picture: 'Picture',
+    create: 'Create',
+    save: 'Save',
+    edit: 'Edit',
+    when: 'When',
+    management: 'Management',
+    login: 'Login',
+    dashboard: 'Cabinet',
+    website: 'Website',
+    exit: 'Logout',
+    logo: 'Developers community DevKG',
+    language: 'Language'
+  },
+  menu: {
+    jobs: 'Vacancies',
+    events: 'Events',
+    meetups: 'Video conferences',
+    community: 'Community',
+    organizations: 'Organizations'
+  },
+  types: {
+    project: 'Project',
+    relocate: 'Relocate',
+    remote: 'Remote work',
+    office: 'Office'
+  },
+  salary: {
+    fixed: 'Fixed payment',
+    hourly: 'Hourly payment',
+    monthly: 'Monthly payment',
+    fixed_formated: '{salary} per project',
+    hourly_formated: '{salary} per hour',
+    monthly_formated: '{salary} per month'
+  },
+  homepage: {
+    titles: {
+      meetups: 'Latest conferences',
+      events: 'Latest events',
+      jobs: 'Latest vacancies'
+    }
+  },
+  footer: {
+    description: 'We are developers community of Kyrgyzstan. This project is supported and managed by members of our community.'
+  },
+  notfound: {
+    page: 'Page cannot found',
+    meetups: 'Video conference cannot be found',
+    jobs: 'Vacancy cannot be found',
+    events: 'Event cannot be found',
+    organizations: 'Organization cannot be found'
+  },
+  meetups: {
+    go: 'Go to broadcast',
+    online: 'Online',
+    upcoming: 'Upcoming conferences',
+    past: 'Passed conferences',
+    video: 'YouTube link or video ID'
+  },
+  jobs: {
+    share: 'Share',
+    spec: 'Vacancy description'
+  },
+  filter: {
+    time: 'By time'
+  },
+  events: {
+    past: 'Passed events'
+  },
+  memberTypes: {
+    owner: 'Owner',
+    member: 'Member',
+    excluded: 'Banned'
+  },
+  organization: {
+    title: 'Organizations',
+    invite: 'Invite a user',
+    invite_title: 'User invitation',
+    invite_create: 'Create invitation',
+    invite_send: 'Send a link',
+    invite_regenerate: 'Re-generate',
+    information: 'Send this link to the user you want to invite: ',
+    username: 'Username:',
+    credentials: 'Role:',
+    make_owner: 'Set as owner',
+    make_member: 'Set as member',
+    remove_description: 'Are you sure you want to remove this organization?',
+    remove_accept: 'Yes',
+    cancel: 'No',
+    exclude: 'Remove'
+  },
+  dashboard: {
+    meetups: {
+      question: 'Would you like to create a conference?',
+      create: 'Create conference',
+      title: 'Your conferences',
+      subtitle: 'Create new conference',
+      sub_description: 'Your video conference will be published on the website and in the Telegram channel after it will be approved by administrators. Advertisement, indecent and not IT related materials must not be published and will be declined.',
+      spec_description: 'You can describe your video conference here, what is going to be discussed and who will talk about it.',
+      picture_description: 'You can upload a background here. It will help you to make your card more attractive',
+      picture_notice: 'Please, do not use pictures with the text, all required information will be fulfilled automatically.',
+      spec_video: 'All meta data will be taken out of video clip. If you are going to create a broadcast earlier, please create it in the YouTube and pate a link here.'
+    },
+    jobs: {
+      description: 'You don\'t have open vacancies. Please set company name, requirements, conditions and salary.',
+      question: 'Would you like to create a vacancy?',
+      create: 'Create vacancy',
+      subtitle: 'Create new vacancy',
+      sub_description: 'Your vacancy will be published on the website and in the Telegram channel, right after administrators approve',
+      title: 'Your vacancies',
+      price_notice: 'Salary range - required field. This will significantly increase your chances to find a good candidate',
+      spec_notice: 'Please describe job conditions, requirements and responsibilities. Also you can briefly describe a company. For example: ',
+      spec_notice_example: '"Our friendly game studio "Bloody Fun" is looking for an experienced project manager"',
+      spec_notice_type: 'Required field. Please choose type of your vacancy.'
+    },
+    events: {
+      description: 'Please choose type of the offline event, for example: a hackathon, a conference, etc.',
+      question: 'Would you like to create?',
+      create: 'Create event',
+      title: 'Your event',
+      subtitle: 'Create new event',
+      sub_description: 'Your event will be published on the website and in the Telegram channel, right after administrators approve. Advertisement, indecent and not IT related materials must not be published and will be declined.',
+      picture_description: 'You can upload a background here. It will help you to make your card more attractive',
+      picture_notice: 'Please, do not use pictures with the text, all required information will be fulfilled automatically.',
+      spec_notice: 'You can describe your event here, what is going to be discussed and who will talk about it.',
+      address: 'Address',
+      registration: 'Registration'
+    },
+    organizations: {
+      description: 'Please create an organization, so you could create your own vacancies, events and vide conferences',
+      question: 'Would you like to create?',
+      create: 'Create organization',
+      title: 'Your organizations',
+      subtitle: 'Create new organizations',
+      subtitle_edit: 'Edit organization',
+      sub_description: 'If organization you want to create already exists, ask the owner for an invite to perform actions on it\'s behalf.',
+      sub_description_edit: 'Each update, it will be revised by an administrator.',
+      picture_description: 'Upload a logo for your organization. It will be used in the list of your organizations.',
+      picture_notice: 'Please upload a logo without background and the same aspect ratio to better display your logo on the site. ',
+      spec_description: 'Please add a brief description of your organization. '
+    }
+  },
+  community: {
+    description: 'If you want to add or remove something, send a Pull request or create an issue in the repository. ',
+    devkg: 'Our chats and channels in Telegram',
+    websites: 'Web sites',
+    github: 'GitHub',
+    facebook: 'Facebook',
+    google_plus: 'Google+',
+    telegram_chats: 'Telegram chats',
+    telegram_channels: 'Телеграм channels',
+    youtube: 'YouTube'
+  }
+};
